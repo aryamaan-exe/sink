0342fd9bfc2395f3523aeab10fffa9f772a6e44c
Aryamaan Goswamy <44227544+aryamaan-exe@users.noreply.github.com>
2024-04-17 14:07:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+users = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    global clipboard
+    if request.method == 'POST':
+        text = request.form['text']
+        clipboard = text
+        password = request.form['password']
+        if password == "$":
+            return f'Text received: {text}'
+        else:
+            return 'Unauthorized: Incorrect password'
+    else:
+        return 'Invalid request method'
+    
+@app.route('/get', methods=['GET'])
+def get():
+    if request.method == 'GET':
+        return clipboard
+
+def handler(event, context):
+    return app(event, context)
